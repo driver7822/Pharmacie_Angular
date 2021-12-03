ca558df65dd2bacb756b9dff60d70291241c9403
Victor <78360638+driver7822@users.noreply.github.com>
2021-12-03 10:36:01 +0100

    Ajout des compléments, Ajout du menu principal, Ajout de la page d'accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
